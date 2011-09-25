2831bd449dfa638c1625d6515de55ae0111ea0a6
Niklas <najklord@gmail.com>
2011-09-25 23:45:57 +0300

    Some fixes.

--- sxgb.sh
+++ sxgb.sh
@@ -40,13 +40,13 @@ S2="refresh"
 		apt-get install python-software-properties pkg-config -y
 		add-apt-repository ppa:team-iquik/xbmc-stable
 		apt-get update
-		apt-get install git-core make g++ gcc gawk pmount libtool nasm yasm automake cmake gperf zip unzip bison libsdl-dev libsdl-image1.2-dev libsdl-gfx1.2-dev libsdl-mixer1.2-dev libfribidi-dev liblzo2-dev libfreetype6-dev libsqlite3-dev libasound-dev python-sqlite libglew-dev libcurl3 libcurl4-gnutls-dev libxrandr-dev libxrender-dev libmad0-dev libogg-dev libvorbisenc2 libsmbclient-dev libmysqlclient-dev libpcre3-dev libdbus-1-dev libhal-dev libhal-storage-dev libjasper-dev libfontconfig-dev libbz2-dev libboost-dev libenca-dev libxt-dev libxmu-dev libpng-dev libjpeg-dev libpulse-dev mesa-utils libcdio-dev libsamplerate-dev libmpeg3-dev libflac-dev libiso9660-dev libass-dev libssl-dev fp-compiler gdc libmpeg2-4-dev libmicrohttpd-dev libmodplug-dev libssh-dev gettext cvs python-dev libyajl-dev libboost-thread-dev libplist-dev python-support ccache libvdpau-dev debhelper libcurl4-openssl-dev x11proto-xinerama-dev libxinerama-dev libfaac-dev libxtst-dev libmms-dev libfaad-dev libwavpack-dev -y -q			
+		apt-get install libplist-dev python-support python-dev ccache libyajl-dev libvdpau-dev debhelper zip git-core make g++ gcc gawk pmount libtool yasm nasm automake cmake gperf gettext unzip bison libsdl-dev libsdl-image1.2-dev libsdl-gfx1.2-dev libsdl-mixer1.2-dev libfribidi-dev liblzo2-dev libfreetype6-dev libsqlite3-dev libogg-dev libasound-dev python-sqlite libglew-dev libcurl3 libcurl4-openssl-dev x11proto-xinerama-dev libxinerama-dev libxrandr-dev libxrender-dev libmad0-dev libogg-dev libvorbisenc2 libsmbclient-dev libmysqlclient-dev libpcre3-dev libdbus-1-dev libhal-dev libhal-storage-dev libjasper-dev libfontconfig-dev libbz2-dev libboost-dev libfaac-dev libenca-dev libxt-dev libxtst-dev libxmu-dev libpng-dev libjpeg-dev libpulse-dev mesa-utils libcdio-dev libsamplerate-dev libmms-dev libmpeg3-dev libfaad-dev libflac-dev libiso9660-dev libass-dev libssl-dev fp-compiler gdc libwavpack-dev libmpeg2-4-dev libmicrohttpd-dev libmodplug-dev libboost-thread-dev -y -q
 		apt-get build-dep xbmc -y
 		mkdir ~/setup
-		cd $HOME/setup	
+		cd $HOME/setup
 		git clone git://github.com/akezeke/spotyxbmc2.git
-		make -C lib/libnfs && sudo make -C lib/libnfs install
 		cd $HOME/setup/spotyxbmc2
+		make -C lib/libnfs && sudo make -C lib/libnfs install
 		cp ~/appkey.h ~/setup/spotyxbmc2 #Copy Spotify appkey
 		./bootstrap; ./configure --prefix=/usr --enable-vdpau --disable-pulse --disable-crystalhd
 		make -j4
@@ -54,7 +54,7 @@ S2="refresh"
 		make install prefix=/usr
 		start xbmc-live
 
-	else 	if [ $1 = $S2 ]
+	else if [ $1 = $S2 ]
 		then
 			stop xbmc-live
 			cd $HOME/setup/spotyxbmc2
@@ -62,10 +62,10 @@ S2="refresh"
 			git reset --hard
 			git clean -xfd
 			git pull --rebase
-            make -C lib/libnfs && sudo make -C lib/libnfs install
+			make -C lib/libnfs && sudo make -C lib/libnfs install
 			./bootstrap ; ./configure --prefix=/usr --enable-vdpau --disable-pulse --disable-crystalhd
 			make -j4
-			make -C lib/addons/script.module.pil
+			make -C $HOME/setup/spotyxbmc2/script.module.pil
 			make install prefix=/usr
 			start xbmc-live
 		else
@@ -78,4 +78,3 @@ S2="refresh"
 		fi
 	fi
 fi
-
